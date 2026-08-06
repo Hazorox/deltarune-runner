2fdaa2115b3c5af459d486ffa5278ff2d90cadcd
Hamza Awad <hamzaawad8899@gmail.com>
2026-08-06 22:05:24 +0300

    Add Hi Score

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,17 +1,19 @@
 extends Node
-@onready var fullscreen=false
-@onready var tower_score = 0
+@onready var fullscreen:bool=false
+@onready var tower_score:int = 0
+@onready var tower_hi:int = 0
 var player:Node2D = null
-# Called when the node enters the scene tree for the first time.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# Listen for full screen
 	if Input.is_action_just_pressed("fullscreen"):
+		# Toggle Based on Boolean
 		if fullscreen:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 			fullscreen=false
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 			fullscreen=true
+	# Listen for Exit
 	elif Input.is_action_just_pressed("exit"):
 		get_tree().change_scene_to_file("res://scenes/menu.tscn")
